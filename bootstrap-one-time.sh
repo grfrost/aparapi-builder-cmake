367e367bc5409ce05d8ece622beca0d1b3323959
grfrost <frost.gary@gmail.com>
2022-07-05 14:28:57 +0100

    add syncleus

--- bootstrap-one-time.sh
+++ bootstrap-one-time.sh
@@ -1,10 +1,11 @@
+cd syncleus
 git clone https://github.com/Syncleus/aparapi.git
 git clone https://github.com/Syncleus/aparapi-jni.git
 git clone https://github.com/Syncleus/aparapi-native.git
 git clone https://github.com/Syncleus/aparapi-examples.git
-
 cp NativeLoader.java.patched aparapi-jni/src/main/java/com/aparapi/natives/NativeLoader.java
 cp JNIExceptions.h.patched aparapi-native/src/cpp/JNIExceptions.h
+cd .. 
 
 mkdir build
 cd build
