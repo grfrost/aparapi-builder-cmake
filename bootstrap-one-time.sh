f580bb86b30be37459eec8a962d53e9d371e417d
grfrost <c9807bc1eddefcbc1d16984e7fa78a0e255aac92>
2022-07-07 19:54:49 +0100

    use tornadovm java and fix aparapi.cpp

--- bootstrap-one-time.sh
+++ bootstrap-one-time.sh
@@ -5,6 +5,7 @@ git clone https://github.com/Syncleus/aparapi-native.git
 git clone https://github.com/Syncleus/aparapi-examples.git
 cp NativeLoader.java.patched aparapi-jni/src/main/java/com/aparapi/natives/NativeLoader.java
 cp JNIExceptions.h.patched aparapi-native/src/cpp/JNIExceptions.h
+cp Aparapi.cpp.patched aparapi-native/src/cpp/runKernel/Aparapi.cpp
 cd .. 
 
 mkdir build
