9ba4db3e05f15bce819e3080582a2de6a4d3b69d
grfrost <c9807bc1eddefcbc1d16984e7fa78a0e255aac92>
2022-07-12 18:22:16 +0100

    cleanup

--- bootstrap-one-time.sh
+++ bootstrap-one-time.sh
@@ -1,24 +1,27 @@
 
 for repo in aparapi aparapi-jni aparapi-native aparapi-examples; do 
    if [ -d ${repo} ]; then 
-      echo ${repo} exists
-   else
-      git clone https://github.com/Syncleus/${repo}.git ${repo}
+      echo ${repo} exists so cleaning
       cd ${repo}
       git checkout . 
       cd ..
+   else
+      echo ${repo} does not exists
+      git clone https://github.com/Syncleus/${repo}.git ${repo} 2>&1 | sed "s/^/   /"
    fi
    if [ -f patches/${repo}.patch ]; then 
+      echo applying patch for ${repo}
       cd ${repo} 
       git apply ../patches/${repo}.patch 
+      cd ..
    else
       echo no patch for ${repo}
    fi
 done 
 
-rmdir -rf build 
+rm -rf build 
 mkdir -p build
 cd build
 cmake .. 
 cd ..
-cmake --build build --target javac --target jar 
+cmake --build build --target javac --target mandel
